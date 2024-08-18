513013918ce94589cfd9b10095c77703a9722769
SURAJ <surajgoswami853@gmail.com>
2024-08-18 11:11:52 +0530

    hello

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const Review = require("./reviews.js");
 const Schema = mongoose.Schema;
+
 
 const listingSchema = new Schema({
   title: {
@@ -11,14 +13,24 @@ const listingSchema = new Schema({
     type: String,
     default:
       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        : v,
+    set: (v) => v === "" ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" : v,
   },
   price: Number,
   location: String,
   country: String,
+  reviews:[
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    }
+  ]
+});
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+  if(listing){
+    await Review.deleteMany({ _id :{ $in : listing.reviews}});
+    console.log("deleteed");
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
